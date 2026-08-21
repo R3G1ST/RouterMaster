0c5a3ee67987dd980f5ed994db82a5ab3a6bfe94
R3G1ST <140413061+R3G1ST@users.noreply.github.com>
2026-08-21 14:23:18 +0500

    fix: кнопка настроек в сайдбаре компактная (36x36px, по центру); версия 1.5.0-beta39

--- router_master.py
+++ router_master.py
@@ -16,7 +16,7 @@ import webbrowser
 import paramiko
 import webview
 
-APP_VERSION = "1.5.0-beta38"
+APP_VERSION = "1.5.0-beta39"
 UPDATE_REPO = "R3G1ST/RouterMaster"
 UPDATE_ASSET = "RouterMaster-Setup.exe"
 
