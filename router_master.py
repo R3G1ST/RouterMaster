cd58ee239fa29e1e5b6aee845bfa6d3ed80b6d6c
R3G1ST <140413061+R3G1ST@users.noreply.github.com>
2026-08-23 05:24:14 +0500

    release: v1.5.2 (rollback to stable)

--- router_master.py
+++ router_master.py
@@ -16,7 +16,7 @@ import webbrowser
 import paramiko
 import webview
 
-APP_VERSION = "1.5.2-beta59"
+APP_VERSION = "1.5.2"
 UPDATE_REPO = "R3G1ST/RouterMaster"
 UPDATE_ASSET = "RouterMaster-Setup.exe"
 
