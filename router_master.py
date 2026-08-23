0d9ca9fe7d92a68cd8554704838d647d9e394be4
R3G1ST <140413061+R3G1ST@users.noreply.github.com>
2026-08-23 05:16:43 +0500

    bump: 1.5.2-beta59

--- router_master.py
+++ router_master.py
@@ -16,7 +16,7 @@ import webbrowser
 import paramiko
 import webview
 
-APP_VERSION = "1.5.1-beta58"
+APP_VERSION = "1.5.2-beta59"
 UPDATE_REPO = "R3G1ST/RouterMaster"
 UPDATE_ASSET = "RouterMaster-Setup.exe"
 
