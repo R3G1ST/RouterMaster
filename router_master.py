2ddd2188d27c3eec7136bbe19c173a1578a34ccb
R3G1ST <140413061+R3G1ST@users.noreply.github.com>
2026-08-30 06:27:47 +0500

    Версия 1.6.5: добавлена поддержка IPK для OpenWrt <24.10

--- router_master.py
+++ router_master.py
@@ -16,7 +16,7 @@ import webbrowser
 import paramiko
 import webview
 
-APP_VERSION = "1.6.4-beta71"
+APP_VERSION = "1.6.5"
 UPDATE_REPO = "R3G1ST/RouterMaster"
 UPDATE_ASSET = "RouterMaster-Setup.exe"
 
